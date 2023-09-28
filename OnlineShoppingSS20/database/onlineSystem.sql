b4e8e2ccd96bb9eadaff0c5a3c0143c8aef30145
Keanghour <126605528+HourZackry@users.noreply.github.com>
2023-09-28 10:06:29 +0700

    Update and rename aprcotstore.sql to onlineSystem.sql

--- OnlineShoppingSS20/database/onlineSystem.sql
+++ OnlineShoppingSS20/database/onlineSystem.sql
@@ -38,9 +38,9 @@ DELIMITER ;
 
 CREATE TABLE `admin_info` (
   `admin_id` int(10) NOT NULL,
-  `admin_name` varchar(100) NOT NULL,
-  `admin_email` varchar(300) NOT NULL,
-  `admin_password` varchar(300) NOT NULL
+  `admin_name` varchar(30) NOT NULL,
+  `admin_email` varchar(150) NOT NULL,
+  `admin_password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -48,7 +48,7 @@ CREATE TABLE `admin_info` (
 --
 
 INSERT INTO `admin_info` (`admin_id`, `admin_name`, `admin_email`, `admin_password`) VALUES
-(1, 'admin', 'admin@apricotstore.com', '25f9e794323b453885f5181f1b624d0b');
+(1, 'admin', 'admin@gmail.com', '25f9e794323b453885f5181f1b624d0b');
 
 -- --------------------------------------------------------
 
